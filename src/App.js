ea0470d3af50b4430af5adab1d3e55867836ac98
breadjoo <yoohwanjoo@nate.com>
2024-07-19 18:03:50 +0900

    0719

--- src/App.js
+++ src/App.js
@@ -25,11 +25,13 @@ function App() {
     {id:3, title:'JavaScript!', body:'JavaScript is ....'}
     ]);
     let content = null;
+    let contextControl = null;
     if(mode === 'Welcome') {
       content=<Article title="Welcome" body="Hello,Web@@@"></Article>
 
     }else if(mode ==='READ') {
-      let title, body = null;
+      let title = null;
+      let body = null;
       for (let i=0; i<topics.length;i++) {
         console.log(topics[i].id, id);
         if(topics[i].id === id) {
@@ -38,6 +40,30 @@ function App() {
         }
       }
       content = <Article title={title} body={body}></Article>
+      contextControl = <>
+       <li>
+          <a href={"/update/"+id} onClick={event=>{
+          event.preventDefault();
+          setMode('UPDATE');
+        }}>Update</a>
+      </li>
+      <li>
+        <button value='delete' onClick={event=>{
+          const newTopics = [];
+          if(window.confirm("삭제하시겠습니까?")){
+          // alert("삭제하시겠습니까?");
+          for(let i=0;i<topics.length;i++){
+            if(topics[i].id !== id) {
+              newTopics.push(topics[i]);
+            }
+          }
+          
+          setTopics(newTopics);
+          setMode("Welcome");
+        }
+        }}>Delete</button>
+      </li>
+      </>
 
     } else if (mode ==='create'){
       content = <Create onCreate={(title,body)=> {
@@ -50,6 +76,28 @@ function App() {
         setNextId(nextId+1);
 
       }}></Create>
+    } else if (mode === 'UPDATE') {
+      let title = null;
+      let body = null;
+      for(let i=0;i<topics.length;i++){
+        if(topics[i].id ===id ) {
+          title = topics[i].title;
+          body = topics[i].body;
+      }
+    }
+      content = <Update title={title} body={body} onUpdate={(title,body)=> {
+        console.log(title, body);
+        const newTopics = [...topics];
+        const updatedTopics = {id:id, title:title, body:body};
+        for(let i=0;i<newTopics.length;i++) {
+          if(newTopics[i].id === id) {
+            newTopics[i] = updatedTopics;
+            break;
+          }
+        }
+        setTopics(newTopics);
+        setMode('READ');
+      }}></Update>
     }
 
   return (
@@ -63,12 +111,15 @@ function App() {
           setId(id);
         }}>  </Nav>
         {content}      
-        <a href='/create' onClick={event=>{
-          event.preventDefault();
-          setMode('create');
-        }}>Create</a>
-      
-      
+        <ul>
+          <li>
+            <a href='/create' onClick={event=>{
+              event.preventDefault();
+              setMode('create');
+            }}>Create</a>
+          </li>
+          {contextControl}
+        </ul>
     </div>
   );
 }
@@ -83,9 +134,32 @@ function Create(props) {
         const body = event.target.body.value;
         props.onCreate(title,body);
       }}>
-        <p><input type='text' name='title' placeholder='title'></input></p>
-        <p><textarea name='body' placeholder='body'></textarea></p>
+        <p><input type='text' name='title' placeholder='title' value={props.title}></input></p>
+        <p><textarea name='body' placeholder='body' value={props.body}></textarea></p>
         <p><input type='submit' value='Create'></input></p>
+      </form>
+    </article>
+  )
+}
+function Update(props) {
+  const [title,setTitle] = useState(props.title);
+  const [body,setBody] = useState(props.body);
+  return(
+    <article>
+      <h2>Update</h2>
+      <form onSubmit={event =>{
+        event.preventDefault();
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        props.onUpdate(title, body);
+      }}>
+        <p><input type='text' name='title' placeholder='title' value={title} onChange={event=> {
+          setTitle(event.target.value);
+        }}></input></p> 
+        <p><textarea name='body' placeholder='body' value={body} onChange={event=> {
+          setBody(event.target.value);
+        }}></textarea></p>
+        <p><input type='submit' value='Update'></input></p>
       </form>
     </article>
   )
